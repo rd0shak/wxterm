2c2182a200bf33a15886d4777bf3f2ae225f834c
rd0shak <nekoninjya@gmail.com>
2026-07-31 21:20:49 -0600

    Some notes on what I want things to look like, and some options.

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 def main():
-    print("Hello from wxterm!")
+    """ 
+    Default output should be simple, the number and degree for temp, some kind
+    of symbols for the conditions. Can use shorthand or emoji, and need to
+    figure out how to pull the data in the first place. I'll also probably need
+    some sort of help or usage information, for features and options!
+    """
+    # Test output
+    print("72°")
 
 
 if __name__ == "__main__":
