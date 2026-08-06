3de06fd438c0d72f4b1fa7c84217d27872cfe6b5
rd0shak <nekoninjya@gmail.com>
2026-08-05 19:23:49 -0600

    I've got a json blob and it's printing to the output. Now I guess I have to do something with it.

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import json
 from urllib.request import urlopen
 
-def query_airport(id):
+def query_airport():
     # https://aviationweather.gov/api/data/metar?ids=KJFK&format=json
     # I need to parse JSON for this, airport queries seems easy enough
-    print("")
+    url = "https://aviationweather.gov/api/data/metar?ids=KJFK&format=json"
+    response = urlopen(url)
+    
+    if response.getcode() == 200:
+        #Decode bytes to string and parse JSON
+        data = json.loads(response.read().decode('utf-8'))
+        print(data)
+    else:
+        print(f"Error: {response.getcode()}")
 
 def main():
     """ 
@@ -17,6 +25,7 @@ def main():
     print("72°")
     print("72°☀️")
     print("☀️")
+    query_airport()
 
 
 if __name__ == "__main__":
